1d7493cef5e0504bcbf00669500b4842726b4d15
Tomasz CEDRO <CeDeROM@tlen.pl>
2017-01-11 17:29:42 +0100

    Settings: localhost access, timezone, templates location.
    Signed-off-by: Tomasz CEDRO <CeDeROM@tlen.pl>

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wjj43j-$@i54pj@#3-^t8(z%u!-)tuen!khf@^91g8c1!*cnoa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/default')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
